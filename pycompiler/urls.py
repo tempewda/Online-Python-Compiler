9e2a7f3b21f178aea710bc9b8c10fa45b073139d
Anukiran Ghosh <59087982+akghosh111@users.noreply.github.com>
2022-10-15 20:34:09 +0530

    first commit

---
+++ pycompiler/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+
+
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="indexpage"),
+    path('runcode', views.runcode, name="runcode"),
+]
